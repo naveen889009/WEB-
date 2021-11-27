5b230950087de31e142bf4e7d8bd4d3ccef4c3a2
naveen889009 <88673024+naveen889009@users.noreply.github.com>
2021-11-27 14:03:10 +0530

    Add files via upload

---
+++ scriptp.js
@@ -0,0 +1,36 @@
+var person = {
+    firstname: "First name of the person",
+    lastname: "last name of person",
+    officeaddress: "office address",
+};
+console.log(person);
+var Person1 = {
+    firstName: "NAVEEN",
+    lastName: "DHAKED",
+    officeaddress: "JAIPUR",
+};
+
+console.log("Person 1");
+console.log(Person1.firstName);
+console.log(Person1.lastName);
+console.log(Person1.officeaddress);
+var Person2 = {
+    firstName: "RAUSHAN",
+    lastName: "KUMAR",
+    officeaddress: "AGRA",
+};
+console.log("Person 2");
+console.log(Person2.firstName);
+console.log(Person2.lastName);
+console.log(Person2.officeaddress);
+var Person3 = {
+    firstName: "RITHVIK",
+    lastName: "KUMAR",
+    officeaddress: "PATNA",
+};
+
+console.log("Person 3");
+console.log(Person3.firstName);
+console.log(Person3.lastName);
+console.log(Person3.officeaddress);
+
